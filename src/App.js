e316eb91db33342f2d1d764a272b45402ad74823
mail <ismail15000000@gmail.com>
2020-08-30 19:23:06 +0700

    firt commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,78 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect, useCallback } from 'react';
+import { gql, useApolloClient } from '@apollo/client';
+
+const EXCHANGE_RATES = gql`
+  query {
+    blogs {
+      id,
+      title,
+      description,
+    }
+  }
+`;
+
+const LOAD_COMMENT = gql`
+  query ($blog_id: Int!){
+    comments(blog_id: $blog_id) {
+      id,
+      comment
+    }
+  }
+`;
 
 function App() {
+  const [blogs, setBlogs] = useState([]);
+  const client = useApolloClient();
+
+  const fectData = useCallback(async () => {
+    const { data } = await client.query({
+      query: EXCHANGE_RATES
+    })
+    setBlogs(data.blogs);
+  }, []);
+
+  useEffect(() => {
+    fectData();
+  },[fectData])
+  
+  async function loadComment(blog_id){
+    const { data } = await client.query({
+      query: LOAD_COMMENT,
+      variables: {blog_id}
+    });
+
+    let newblogs = blogs.map(blog => blog.id === blog_id ? {...blog, comments: data.comments} : {...blog});
+    setBlogs(newblogs);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+    {
+      blogs.map(blog => (
+        <div key={blog.id}>
+          <h1>{blog.title}</h1>
+          <p>{blog.description}</p>
+          <button onClick={() => loadComment(blog.id)}>Load komen</button>
+          {
+            blog.comments
+            ? <div>
+                <ul>
+                  {
+                    blog.comments.map(comment => (
+                      <li key={comment.id}>{comment.comment}</li>
+                    ))
+                  }
+                </ul>
+              </div>
+            : null
+          }
+          
+        </div>
+      ))
+    }
     </div>
-  );
+  )
+  
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
+
+const client = new ApolloClient({
+  uri: 'http://localhost:3000/graphql',
+  cache: new InMemoryCache()
+});
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ApolloProvider client={client}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ApolloProvider>
+  ,
   document.getElementById('root')
 );
 
